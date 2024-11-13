a80550c006f3fd914f0901b2ab215198a280f8db
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-11-13 07:30:36 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Telegram bot API details
+API_ID = "your_telegram_api_id"
+API_HASH = "your_telegram_api_hash"
+BOT_TOKEN = "your_telegram_bot_token"
+
+# Crunchyroll credentials
+CR_USERNAME = "your_crunchyroll_username"
+CR_PASSWORD = "your_crunchyroll_password"
